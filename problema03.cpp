c1d8fe7ef22bbf6652b91c85c249adf6431bf58e
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 18:04:49 -0500

    subo el comienzo del problema03

---
+++ problema03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+const int F=4;
+const int C=5;
+
+int main(){
+
+    int A[F][C]={
+        {3,3,3,3,3},
+        {3,4,4,2,3},
+        {3,4,5,4,3},
+        {3,3,3,3,3}
+    };
+
+    return 0;
+}
