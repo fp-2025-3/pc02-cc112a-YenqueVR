ad217e0efa48b23b9bb40d3addbcace5f6be6ec9
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 18:24:24 -0500

    implementacion para imprimir la matriz con punteros

--- problema03.cpp
+++ problema03.cpp
@@ -4,6 +4,8 @@ using namespace std;
 const int F=4;
 const int C=5;
 
+void imprimirMatrizYpicos(int (*inicio)[C], int (*fin)[C]);
+
 int main(){
 
     int A[F][C]={
@@ -13,5 +15,22 @@ int main(){
         {3,3,3,3,3}
     };
 
+    int (*inicio)[C]=A; //primera fila
+    int (*fin)[C]=A+F;  //la fila inmediatamente despues de la ultima fila del arreglo
+
+    imprimirMatrizYpicos(inicio,fin);
+
     return 0;
+}
+
+void imprimirMatrizYpicos(int (*inicio)[C], int (*fin)[C]){
+    cout<<"\nMatriz A["<<F<<"]["<<C<<"]:  ";
+    for(int (*i)[C]=inicio; i<fin; i++){    //desde la primera fila hasta la ultima
+        if(i!=inicio) cout<<"                 ";
+        for(int *j=*i; j<*i+C; j++){    //*i = A[*][C], *j = A[F][C]
+            cout<<*j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
 }
