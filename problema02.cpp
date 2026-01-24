49225ce49aac1300a2505ff1d9332bd85cc8ab6a
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 15:25:07 -0500

    problema02 terminado, varias correcciones aplicadas

--- problema02.cpp
+++ problema02.cpp
@@ -25,32 +25,32 @@ int main(){
     bool (*eq)(int,int)=igual;
     int *ptr=buscar(inicio,fin,eq,clave);
     cout<<"\nIgualdad exacta: ";
-    if(ptr!=nullptr) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
-    else cout<<"\nNo encontrado";
+    if(ptr!=fin) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
+    else cout<<"No encontrado";
 
     eq=mismaParidad;
-    int *ptr=buscar(inicio,fin,eq,clave);
+    ptr=buscar(inicio,fin,eq,clave);
     cout<<"\nMisma paridad: ";
-    if(ptr!=nullptr) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
-    else cout<<"\nNo encontrado";
+    if(ptr!=fin) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
+    else cout<<"No encontrado";
 
     eq=divideA;
-    int *ptr=buscar(inicio,fin,eq,clave);
+    ptr=buscar(inicio,fin,eq,clave);
     cout<<"\nDivide a: ";
-    if(ptr!=nullptr) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
-    else cout<<"\nNo encontrado";
+    if(ptr!=fin) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
+    else cout<<"No encontrado";
 
     eq=mismaCantidadDigitos;
-    int *ptr=buscar(inicio,fin,eq,clave);
+    ptr=buscar(inicio,fin,eq,clave);
     cout<<"\nMisma cantidad de digitos: ";
-    if(ptr!=nullptr) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
-    else cout<<"\nNo encontrado";
+    if(ptr!=fin) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
+    else cout<<"No encontrado";
 
     eq=mismoResiduoMod5;
-    int *ptr=buscar(inicio,fin,eq,clave);
+    ptr=buscar(inicio,fin,eq,clave);
     cout<<"\nMismo residuo modulo 5: ";
-    if(ptr!=nullptr) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
-    else cout<<"\nNo encontrado";
+    if(ptr!=fin) cout<<"encontrado valor: "<<*ptr<<" Pos: "<<ptr;
+    else cout<<"No encontrado";
 
     cout<<endl;
     return 0;
@@ -64,28 +64,33 @@ void imprimirArreglo(int *inicio, int *fin){
 }
 
 int* buscar(int *inicio, int *fin, bool (*equivale)(int,int), int clave){
-    
+    for(int *i=inicio; i<fin; i++){
+        if(equivale(*i, clave)) return i;   //si sale verdadero, retorna la posicion
+    }
+    return fin; //caso contrario si no lo encuentra, devuelve fin
 }
 bool igual(int a, int b){
     return a==b;
 }
 bool mismaParidad(int a, int b){
-    return ((a%2==0 && b%2==0)||(a%2==1 && b%2==1));
+    if(a<0) a*=-1;  //vuelve positivo para comparar correctamente
+    if(b<0) b*=-1;
+    return ((a%2==0 && b%2==0)||(a%2==1 && b%2==1));    //ambos pares o impares
 }
 bool divideA(int a, int b){
-    return b%a==0;
+    return b%a==0;  //si su division es exacta
 }
 bool mismaCantidadDigitos(int a, int b){
-    if(a<0) a*=-1;
+    if(a<0) a*=-1;  //lso vuelve positivo para comparar mejor
     if(b<0) b*=-1;
 
-    int digA=0, digB=0;
+    int digA=0, digB=0; //los digitos de a y b
 
-    if(a==0) digA++;
+    if(a==0) digA++;    //0 es de un digito
     else{
-        while(a>0){
-            digA++;
-            a/=10;
+        while(a>0){ //mientras a no este en 0<=a<1, ~a=0
+            digA++; //sumamos un digito para a
+            a/=10;  //dividimos para obtener el siguiente digito
         }
     }
 
@@ -97,8 +102,8 @@ bool mismaCantidadDigitos(int a, int b){
         }
     }
 
-    return digA==digB;
+    return digA==digB;  
 }
 bool mismoResiduoMod5(int a, int b){
-    return (a%5)==(b%5);
+    return (a%5)==(b%5);    //verdadero cuando sus residuos de dividir con 5 son iguales
 }
