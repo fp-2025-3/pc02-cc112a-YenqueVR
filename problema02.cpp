0e01a4489c95ad36793e0dbf456906ebbe5d8890
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 14:28:05 -0500

    pequeño cambio en funcion divideA(int, int)

--- problema02.cpp
+++ problema02.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+void imprimirArreglo(int *inicio, int *fin);
 int* buscar(int *inicio, int *fin, bool (*equivale)(int,int), int clave);
 bool igual(int a, int b);
 bool mismaParidad(int a, int b);
@@ -12,13 +13,15 @@ int main(){
 
     int arr[]={-1,21,2,-2,8,13,25};
     int N=sizeof(arr)/sizeof(arr[0]);
-
     int *inicio=arr;
     int *fin=arr+N;
-
     int clave=13;
 
     return 0;
+}
+
+void imprimirArreglo(int *inicio, int *fin){
+
 }
 
 int* buscar(int *inicio, int *fin, bool (*equivale)(int,int), int clave){
@@ -31,12 +34,14 @@ bool mismaParidad(int a, int b){
     return ((a%2==0 && b%2==0)||(a%2==1 && b%2==1));
 }
 bool divideA(int a, int b){
-
+    return b%a==0;
 }
 bool mismaCantidadDigitos(int a, int b){
     if(a<0) a*=-1;
     if(b<0) b*=-1;
+
     int digA=0, digB=0;
+
     if(a==0) digA++;
     else{
         while(a>0){
@@ -44,6 +49,7 @@ bool mismaCantidadDigitos(int a, int b){
             a/=10;
         }
     }
+
     if(b==0) digB++;
     else{
         while(b>0){
