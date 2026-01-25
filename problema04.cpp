d09f97680dda43eadcbeaddc3b9e02b76c0a0c25
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 20:32:54 -0500

    avance con la implementacion de sumaSubmatriz()

--- problema04.cpp
+++ problema04.cpp
@@ -19,6 +19,25 @@ int main(){
     return 0;
 }
 
-int sumaSubmatriz(int (*i)[C], int FILAS, int COL, int *pos1, int *pos2){
+int sumaSubmatriz(int (*inicio)[C], int FILAS, int COL, int *pos1, int *pos2){
+    int (*f1)[COL]=inicio, (*f2)[COL]=inicio;
+    int *c1=*inicio, *c2=*inicio;
 
+    //hallar la posicion exacta en la matriz de las posiciones 
+    for(int (*i)[COL]=inicio; i<inicio+FILAS; i++){
+        for(int *j=*i; j<*i+COL; j++){
+            if(j==pos1) f1=i; c1=j; //se hallo la posicion exacta de pos1 (i1, j1)
+            if(j==pos2) f2=i; c2=j; //se hallo la posicion exacta de pos2 (i2, j2)
+        }
+    }
+
+    //calcular la suma de elementos de la submatriz delimitada
+    int suma=0;
+    for(int (*i)[COL]=f1; i<=f2; i++){  //desde la fila f1 hasta la fila f2
+        for(int *j=*(i+(c1-*i)); j<*(i+(c2-*i)); j++){  //*(i+(c1-*i)): Pos del elemento en la fila i, c1-*i= #columna
+            suma+= *j;
+        }
+    }
+
+    return suma;    //retorna la suma de los elementos de la submatriz
 }
