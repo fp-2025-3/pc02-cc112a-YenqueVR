9d25e98f8faa8abb6569f684ecd1b89688f4745d
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 21:09:54 -0500

    avances en la funcion sumaSubmatriz()

--- problema04.cpp
+++ problema04.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int F=4;
 const int C=5;
 
-int sumaSubmatriz(int (*i)[C], int FILAS, int COL, int *pos1, int *pos2);
+int sumaSubmatriz(int (*i)[C], int *pos1, int *pos2);
 
 int main(){
     
@@ -15,28 +15,41 @@ int main(){
         { 1,-2, 3, 4,-1}
     };
     int (*inicio)[C]=A;
+    int *pos1=*inicio;
+    int *pos2=(*(inicio+F-1))+C-2;
 
+    cout<<"\nSuma de submatriz: "<<sumaSubmatriz(inicio,pos1,pos2)<<endl;
     return 0;
 }
 
-int sumaSubmatriz(int (*inicio)[C], int FILAS, int COL, int *pos1, int *pos2){
-    int (*f1)[COL]=inicio, (*f2)[COL]=inicio;
+int sumaSubmatriz(int (*inicio)[C], int *pos1, int *pos2){
+    int (*f1)[C]=inicio, (*f2)[C]=inicio;
     int *c1=*inicio, *c2=*inicio;
 
     //hallar la posicion exacta en la matriz de las posiciones 
-    for(int (*i)[COL]=inicio; i<inicio+FILAS; i++){
-        for(int *j=*i; j<*i+COL; j++){
-            if(j==pos1) f1=i; c1=j; //se hallo la posicion exacta de pos1 (i1, j1)
-            if(j==pos2) f2=i; c2=j; //se hallo la posicion exacta de pos2 (i2, j2)
+    for(int (*i)[C]=inicio; i<inicio+F; i++){
+        for(int *j=*i; j<*i+C; j++){
+            if(j==pos1){
+                f1=i; c1=j; //se hallo la posicion exacta de pos1 (i1, j1)
+                cout<<"\nF1: "<<i-inicio;
+                cout<<"\nC1: "<<j-*i;
+            }
+            if(j==pos2){
+                f2=i; c2=j; //se hallo la posicion exacta de pos2 (i2, j2)
+                cout<<"\nF2: "<<i-inicio;
+                cout<<"\nC2: "<<j-*i<<endl;
+            }
         }
     }
 
     //calcular la suma de elementos de la submatriz delimitada
     int suma=0;
-    for(int (*i)[COL]=f1; i<=f2; i++){  //desde la fila f1 hasta la fila f2
-        for(int *j=*(i+(c1-*i)); j<*(i+(c2-*i)); j++){  //*(i+(c1-*i)): Pos del elemento en la fila i, c1-*i= #columna
+    for(int (*i)[C]=f1; i<=f2; i++){  //desde la fila f1 hasta la fila f2
+        for(int *j=*i+(c1-*i); j<=*i+(c2-*i); j++){  //*i+(c1-*i): Pos del elemento en la fila i, c1-*i= #columna
             suma+= *j;
+            cout<<*j<<" ";
         }
+        cout<<endl;
     }
 
     return suma;    //retorna la suma de los elementos de la submatriz
