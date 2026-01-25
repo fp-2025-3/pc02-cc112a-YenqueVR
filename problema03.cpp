aea53758a520147170ad1551348622c2cb406eea
Yenque VR <yenque.vega.r@uni.pe>
2026-01-24 19:24:33 -0500

    posicionPico() e imprimir los picos completado

--- problema03.cpp
+++ problema03.cpp
@@ -24,19 +24,31 @@ int main(){
     return 0;
 }
 
-int *posicionPico(int (*inicio)[C], int (*fin)[C], int (*i)[C], int *j){
+int *posicionPico(int (*inicio)[C], int (*fin)[C], int (*i)[C], int *j){    //int (*i)[C]: fila i, int *j: pos en fila i
     bool alMenosMayor=false;    //al menos a uno debe ser mayor de sus vecino
     bool mayorOigual=true;  //debe ser mayor o igual que sus vecinos
     //comparar arriba
     if(i-1>=inicio && *(*(i-1)+(j-*i))<=*j){    //i-1: fila anterior, j-*i: posicion columna desde el comienzo de fila i 
         alMenosMayor=true;
     }else if(i!=inicio) mayorOigual=false;  //no modifica nada si esta en la fila = inicio
+
     //abajo
     if(i+1<fin && *(*(i+1)+(j-*i))<=*j){ 
         alMenosMayor=true;
     }else if(i!=fin-1) mayorOigual=false;   //no modifica nada si esta en la fila = fin-1
+
     //izquierda
-    
+    if(j-1>=*i && *(j-1)<=*j){  //j-1: elemento anterior, *(j-1): valor del elemento anterior
+        alMenosMayor=true;
+    }else if(j!=*i) mayorOigual=false;  //no modifica nada si esta en la columna = *i
+
+    //derecha
+    if(j+1<*i+C && *(j+1)<=*j){
+        alMenosMayor=true;
+    }else if(j!=*i+C-1) mayorOigual=false;  //no modifica nada si esta en la columna = *i
+
+    if(alMenosMayor && mayorOigual) return j;   //si cumple, retorna la direccion o posicion del elemento pico
+    else return *fin;   //si no cumple, retorna la direccion *fin = *(A+F) o *(inicio+F)
 }
 
 void imprimirMatrizYpicos(int (*inicio)[C], int (*fin)[C]){
@@ -50,7 +62,16 @@ void imprimirMatrizYpicos(int (*inicio)[C], int (*fin)[C]){
         cout<<endl;
     }
     cout<<endl;
-    int (*i)[C]=inicio+1;
-    int *j=*i+1;
-    cout<<*(*(i-1)+(j-*i))<<endl;
+
+    cout<<"Elementos pico:\n";
+    for(int (*i)[C]=inicio; i<fin; i++){
+        for(int *j=*i; j<*i+C; j++){
+            int *elemPico=posicionPico(inicio,fin,i,j);
+            if(elemPico!=*fin){
+                cout<<"Pico en ("<<i-inicio<<", "<<j-*i<<"): "<<*elemPico<<" ; Pos: "<<elemPico;
+                cout<<endl;
+            }
+        }
+    }
+    cout<<endl;
 }
