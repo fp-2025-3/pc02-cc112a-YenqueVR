a89e54432887c5cc46f29dcbd2d466cf57204191
PCS5 <scptx0@users.noreply.github.com.>
2026-01-28 16:57:07 -0500

    ejercicio1 terminado

--- ejercicio02.cpp
+++ ejercicio02.cpp
@@ -7,14 +7,22 @@ void clasificar(char* palabras[], int n, char* cortas[], int& nc, char* largas[]
 int comparar_palabras(const char* a, const char* b);
 void ordenar(char* v[], int n);
 void imprimir_grupo(const char* titulo, char* v[], int n);
+void intercambiar(char *&a, char *&b);
 
 int main(){
     char texto[300]="Programar en C++ requiere logica C++ exige disciplina";
     char* palabras[60];
     char* cortas[60];
     char* largas[60];
+    int nc,nl;
 
-
+    int n=separar_palabras(texto,palabras);
+    clasificar(palabras,n,cortas,nc,largas,nl);
+    ordenar(cortas,nc);
+    ordenar(largas,nl);
+    imprimir_grupo("cortas",cortas,nc);
+    cout<<endl;
+    imprimir_grupo("largas",largas,nl);
 
     return 0;
 }
@@ -22,10 +30,9 @@ int main(){
 int separar_palabras(char* texto, char* palabras[]){
     char *p=texto;  //puntero al primer caracter del texto
     int contPalabras=0;
-    for(; *p='\0'; p++){    //hasta terminar el texto
-        if(*p!=' ' && (p==texto || *(p-1)!=' ')){
-            *palabras=p;    //almacenamos el puntero al primer caracter de la palabra
-            palabras++; //avanzamos para almacenar el siguiente puntero
+    for(; *p!='\0'; p++){    //hasta terminar el texto
+        if(*p!=' ' && (p==texto || *(p-1)==' ')){
+            palabras[contPalabras]=p;    //almacenamos el puntero al primer caracter de la palabra
             contPalabras++; //aumentamos el numero de palabras que hay en el texto
         }
     }
@@ -68,12 +75,24 @@ void ordenar(char* v[], int n){
     for(char **i=v; i<v+n-1; i++){
         for(char **j=v; j<v+n-(i-v)-1; j++){
             if(comparar_palabras(*j,*(j+1))>0){
-                swap(*j,*(j+1));
+                intercambiar(*j,*(j+1));
             }
         }
     }
 }
 
+void intercambiar(char *&a, char *&b){
+    char *temp=a;
+    a=b;
+    b=temp;
+}
+
 void imprimir_grupo(const char* titulo, char* v[], int n){
     cout<<"\nPalabras "<<titulo<<endl;
+    for(char **i=v; i<v+n; i++){
+        for(char *j=*i; *j!='\0' && *j!=' '; j++){
+            cout<<*j;
+        }
+        cout<<endl;
+    }
 }
