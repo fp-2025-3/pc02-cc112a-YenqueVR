22570baef49be915e57e6edbbc7ec0a1ab93d758
PCS5 <scptx0@users.noreply.github.com.>
2026-02-03 15:15:08 -0500

    nuevos archivos semana 4

---
+++ Semana04/ejemplo01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int *ptr = new int[100];
+    for(int i=0; i<100; i++){
+        ptr[i]=i+1;
+    }
+    cout<<"\nbalabanahf\n";
+    
+}
