a5b22b1a3be3f770654369aeeb6e2258fff72010
PCS5 <scptx0@users.noreply.github.com.>
2026-02-03 17:13:13 -0500

    ejercicio1 de clase 1 terminado

--- Semana04/ejemplo01.cpp
+++ Semana04/ejemplo01.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 
 int main(){
-    int *ptr = new int[100];
-    for(int i=0; i<100; i++){
-        ptr[i]=i+1;
-    }
-    cout<<"\nbalabanahf\n";
+    int *ptr = new int(6);
+    cout<<ptr;
+    delete ptr;
     
 }
