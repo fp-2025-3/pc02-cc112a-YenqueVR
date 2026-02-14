af0919281af75f1545bb885e7d4f2e1cce1a58be
unknown <yenque.vega.r@uni.pe>
2026-02-14 08:42:31 -0500

    pequeñas modificaciones y comentarios del problema 2

--- Semana05/PC4/problema2.cpp
+++ Semana05/PC4/problema2.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 struct Seleccion {
-    string nombre;
-    int PG,PE,PP,GF,GC;
-    int puntaje,DG;
-    float rend;
+    string nombre; //nombre de la seleccion
+    int PG,PE,PP,GF,GC; //partidos ganados, empatados, perdidos, gole favor y en contra
+    int puntaje,DG; //puntaje y diferencia de goles (DG=GF-GC)
+    float rend; //rendimiento (rend(%)=100*(puntaje/max_puntaje)
 };
 
-Seleccion *generarSelecciones(int n, int pj);
-void tabla(Seleccion *s, int n);
-void ordenarSeleccion(Seleccion *&s, int n);
+Seleccion *generarSelecciones(int n, int pj);   //genera el arreglo de n selecciones
+void tabla(Seleccion *s, int n);    //muestra los datos de las selecciones y campeon
+void ordenarSeleccion(Seleccion *&s, int n);    //
 void seleccionCampeon(Seleccion *s);
 void liberarMemoria(Seleccion *&s);
 
@@ -21,8 +21,8 @@ int main(){
     int n=5, pj=100;
     Seleccion *sel=generarSelecciones(n,pj);
     ordenarSeleccion(sel,n);
+
     tabla(sel,n);
-    seleccionCampeon(sel);
     liberarMemoria(sel);
 
     return 0;
@@ -41,9 +41,8 @@ Seleccion *generarSelecciones(int n, int pj){
         getline(cin,s[i].nombre);
 
         for(int j=0; j<pj; j++){    //los 100 partidos simulados, pj=100
-            int gf,gc;  //goles a favor y en contra por cada partido
-            gf=rand()%6;
-            gc=rand()%6;   //de 0 a 5 goles cada uno
+            //goles a favor y en contra por cada partido
+            int gf=rand()%6, gc=rand()%6;   //de 0 a 5 goles cada uno
 
             s[i].GF+=gf;    //acumulamos los goles a favor y en contra
             s[i].GC+=gc;
@@ -59,47 +58,48 @@ Seleccion *generarSelecciones(int n, int pj){
 
     }
 
-    return s;
+    return s;   //retornamos el arreglo de selecciones
 }
 
 void ordenarSeleccion(Seleccion *&s, int n){
-    for(int i=0; i<n-1; i++){
+    for(int i=0; i<n-1; i++){   //ordenamiento burbuja
         for(int j=0; j<n-i-1; j++){
-            if(s[j].puntaje==s[j+1].puntaje){
-                if(s[j].DG==s[j+1].DG){
+            if(s[j].puntaje==s[j+1].puntaje){   //si los puntajes son lo mismo, entonces
+                if(s[j].DG==s[j+1].DG){ //si incluso la diferencia de goles son iguales
                     if(s[j].GF<s[j+1].GF){
-                        swap(s[j],s[j+1]);
+                        swap(s[j],s[j+1]);  //cambio por goles a favor (GF)
                     }
-                }else if(s[j].puntaje<s[j+1].puntaje){
-                    swap(s[j],s[j+1]);
+                }else if(s[j].DG<s[j+1].DG){
+                    swap(s[j],s[j+1]);  //cambio por diferencia de goles (DG)
                 }
             }else if(s[j].puntaje<s[j+1].puntaje){
-                swap(s[j],s[j+1]);
+                swap(s[j],s[j+1]);  //cambio por puntaje
             }
         }
     }
 }
 
 void tabla(Seleccion *s, int n){
-    cout<<fixed<<setprecision(1);
-    cout<<left;
+    cout<<fixed<<setprecision(1);   //precision de 1 decimal
+    cout<<left<<endl;
 
-    cout<<setw(20)<<"Seleccion"<<setw(5)<<"PG"<<setw(5)<<"PE"<<setw(5)<<"PP";
+    cout<<setw(20)<<"Seleccion"<<setw(5)<<"PG"<<setw(5)<<"PE"<<setw(5)<<"PP"; //encabezado de la tabla
     cout<<setw(5)<<"GF"<<setw(5)<<"GC"<<setw(5)<<"DG"<<setw(7)<<"Pts"<<"Rend(%)";
     cout<<"\n-----------------------------------------------------------------------\n";
 
     for(int i=0; i<n; i++){
-        cout<<setw(20)<<s[i].nombre<<setw(5)<<s[i].PG<<setw(5)<<s[i].PE<<setw(5)<<s[i].PP;
+        cout<<setw(20)<<s[i].nombre<<setw(5)<<s[i].PG<<setw(5)<<s[i].PE<<setw(5)<<s[i].PP;  //datos de la seleccion i
         cout<<setw(5)<<s[i].GF<<setw(5)<<s[i].GC<<setw(5)<<s[i].DG<<setw(7)<<s[i].puntaje<<s[i].rend;
         cout<<endl;
     }
-    cout<<endl;
+
+    seleccionCampeon(s);    //mostramos al campeon de las selecciones
 }
 
-void seleccionCampeon(Seleccion *s){
+void seleccionCampeon(Seleccion *s){    //al ordenar, el campeon esta primero, en s[0] o *s
     cout<<"\nCAMPEON: "<<s->nombre<<" | Puntaje: "<<s->puntaje<<" | DG: "<<s->DG<<" | Rendimiento: "<<s->rend<<"%\n";
 }
-void liberarMemoria(Seleccion *&s){
+void liberarMemoria(Seleccion *&s){ //libera memoria dinamica del arreglo de selecciones
     delete[] s;
     s=nullptr;
 }
