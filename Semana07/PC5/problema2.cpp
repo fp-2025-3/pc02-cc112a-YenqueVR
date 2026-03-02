ec334927a007cea6ffd60bfb8394bce06bc5e3c6
unknown <yenque.vega.r@uni.pe>
2026-03-02 00:04:45 -0500

    problema 2. funcion totalEmpleados eliminado, y modificaciones para la nueva adaptacion

--- Semana07/PC5/problema2.cpp
+++ Semana07/PC5/problema2.cpp
@@ -9,10 +9,9 @@ struct Empleado {
 };
 
 void buscarEmpleadoPorID(const char *archivo, int IDbuscado);
-int totalEmpleados(const char *archivo);
 void mostrarEmpleado(Empleado e);
 bool modificarSalario(Empleado &e);
-void mostrarTotalEmpleados(const char *archivo, int n);
+void mostrarTotalEmpleados(const char *archivo);
 
 int main(){
     int IDbuscado;
@@ -26,9 +25,8 @@ int main(){
 }
 
 void buscarEmpleadoPorID(const char *archivo, int IDbuscado){
-    int numEmpleado=totalEmpleados(archivo);
-    if(IDbuscado<=0 || IDbuscado>numEmpleado){
-        cerr<<"\nNo existe empleado con ese numero de ID.\n";
+    if(IDbuscado<=0){
+        cerr<<"\nEl ID de los empleados es mayor a 0. Busqueda cancelada.\n";
         return;
     }
 
@@ -38,38 +36,30 @@ void buscarEmpleadoPorID(const char *archivo, int IDbuscado){
         return;
     }
 
-    mostrarTotalEmpleados(archivo,numEmpleado);
+    int posicionEmpleado=(IDbuscado-1) * sizeof(Empleado);  //posicion del comienzo de la estructura del empleado buscado
 
-    file.seekg((IDbuscado-1) * sizeof(Empleado), ios::beg); //posicionamos el puntero de lectura en el empleado buscado
+    file.seekg(posicionEmpleado, ios::beg); //posicionamos el puntero de lectura en el empleado buscado
 
-    Empleado empleadoBuscado;
+    Empleado empleadoBuscado;   //estructura creada para almacenar al empleado buscado
     file.read((char*)&empleadoBuscado, sizeof(Empleado));
-    file.clear();
+
+    if(file.eof()){ //si llega al final o mas alla, detectara lectura invalida y saldra de la funcion
+        file.clear();
+        file.close();  
+        cerr<<"\nNo se encontro un empleado con el ID proporcionado.\n";
+        return;
+    }
 
     mostrarEmpleado(empleadoBuscado);   //mostramos al empleado
     if(!modificarSalario(empleadoBuscado)) return;  //modificamos el salario
     
-    file.seekp((IDbuscado-1) * sizeof(Empleado), ios::beg); //posicionamos el puntero de escritura en el empleado a modificar
+    file.seekp(posicionEmpleado, ios::beg); //posicionamos el puntero de escritura en el empleado a modificar
     file.write((char*)&empleadoBuscado, sizeof(Empleado));
     file.close();
 
-    cout<<"\nArchivo modificado exitosamente.\n";
+    cout<<"\nSalario actualizado correctamente.\n";
 
-    mostrarTotalEmpleados(archivo,numEmpleado);
-}
-
-int totalEmpleados(const char *archivo){
-    ifstream file(archivo, ios::binary);
-    if(!file){
-        cerr<<"\nNo se pudo abrir output/empleados.dat";
-        return -1;
-    }
-
-    int numEmpleados=0;
-    for(Empleado e; file.read((char*)&e, sizeof(e)); numEmpleados++);
-
-    file.close();
-    return numEmpleados;
+    mostrarTotalEmpleados(archivo);     //despues de si es que se modifica el salario
 }
 
 void mostrarEmpleado(Empleado e){
@@ -84,34 +74,30 @@ bool modificarSalario(Empleado &e){
     cout<<"\nIngrese nuevo salario: "; cin>>nuevoSalario;
 
     if(nuevoSalario<0){
-        cerr<<"\nEl sb moalario no puede ser negativo. Operacion cancelada.\n";
+        cerr<<"\nEl salario no puede ser negativo. Operacion cancelada.\n";
+        return false;
+    }else if(nuevoSalario==e.salario){
+        cerr<<"\nEl salario no esta modificandose realmente. Operacion cancelada.\n";
         return false;
     }
 
-    e.salario=nuevoSalario;
-    cout<<"\nSalario actualizado correctamente.\n";
+    e.salario=nuevoSalario; //si es valido, el salario es modificado
 
     return true;
 }
 
-void mostrarTotalEmpleados(const char *archivo, int n){
+void mostrarTotalEmpleados(const char *archivo){
     ifstream file(archivo, ios::binary);
     if(!file){
         cerr<<"\nNo se pudo abrir output/empleados.dat";
         return;
     }
 
-    Empleado *e=new Empleado[n];
-    file.read((char*)e, sizeof(Empleado)*n);
-
     cout<<"\nEmpleados:\n";
-    for(int i=0; i<n; i++){
-        cout<<"ID: "<<e[i].id<<" | Nombre: "<<e[i].nombre<<" | Salario: "<<e[i].salario<<endl;
+    for(Empleado e; file.read((char*)&e, sizeof(e));){  //lee cada empleado hasta el final
+        cout<<"ID: "<<e.id<<" | Nombre: "<<e.nombre<<" | Salario: "<<e.salario<<endl;
     }
     cout<<endl;
 
     file.close();
-
-    delete[] e; //liberamos memoria
-    e=nullptr;
 }
